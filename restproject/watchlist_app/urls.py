f3fb8b072d42876ab36fc8b056d4ccb0ab720c3b
ErenAydogan <103932990+ErenAydogan@users.noreply.github.com>
2024-03-14 02:35:26 +0300

    Django

--- restproject/watchlist_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'watchlist'
-
-urlpatterns = [
-    path('', views.movie_list, name="movie_list"),
-    path('<int:pk>/', views.movie_details, name='detail')
-]
