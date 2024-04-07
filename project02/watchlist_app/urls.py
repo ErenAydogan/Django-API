f1332fbadf8377bfe53ae5ee96d80c7d596c7094
ErenAydogan <erenaydogan@live.fr>
2024-04-08 01:15:49 +0300

    Deleted

--- project02/watchlist_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'watchlist'
-
-urlpatterns = [
-    path('list/', views.movie_list, name="movie-list"),
-    path('<int:pk>/', views.movie_details, name="movie-details"),
-]
