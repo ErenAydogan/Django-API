c5e9777979a32bba1538fe32acc390c68415f6b7
ErenAydogan <erenaydogan@live.fr>
2024-04-22 21:57:29 +0300

    Updated

--- project01/api/serializers.py
+++ project01/api/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        ##fields = ['id', 'name', 'description']
+        #exclude = []
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different!")
+        else:
+            return data
+        
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short!")
+        
+        else:
+            return value
+
+
+
+
+"""
 def name_length(value):
     if len(value) < 2:
         raise serializers.ValidationError("Name is too short!")
@@ -27,7 +51,7 @@ class MovieSerializer(serializers.Serializer):
         else:
             return data
 
-"""
+
     def validate_name(self, value):
         if len(value) < 2:
             raise serializers.ValidationError("Name is too short")
